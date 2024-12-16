06311a4de885f3accde7c892839503ad514a0e50
firstlast-0 <127908930+firstlast-0@users.noreply.github.com>
2024-12-16 14:39:47 +0800

    Add Remove from cart feature

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,31 +1,51 @@
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, Link } from "react-router-dom";
 
 function Cart() {
     const [cart, setCart] = useOutletContext();
     let total = 0;
+    let totalItems = 0;
     for (let item of cart) {
         total += item.amount * item.price;
     }
+    for (let item of cart) {
+        totalItems += item.amount;
+    }
+
+    function removeHandler(index) {
+        let cartCopy = cart.slice();
+        cartCopy.splice(index, 1);
+        setCart(cartCopy);
+    }
 
     return (
-        <div>
-            <h1>Cart</h1>
-            <h2>Total: {total}</h2><br />
-            <button onClick={() => {setCart([])}} disabled>Checkout</button>
-            <br /><br />
-            {cart.map((item, index) => {
-                return (
-                    <div className="item" key={index}>
-                        <img src={item.img} alt={item.name} />
+        <div className="cart">
+            {cart.length > 0 ?
+                <div>
+                    {cart.map((item, index) => {
+                        return (
+                            <div className="item" key={index}>
+                                <img src={item.img} alt={item.name} />
+                                <div>
+                                    <h2>{item.name}</h2><br />
+                                    Quantity: <h3>{item.amount}</h3><br />
+                                    Total Price: <h3>${item.price * item.amount}</h3><br />
+                                    <button onClick={() => {removeHandler(index)}}>Remove</button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            : <div className="cartInfo"><h1>Your Cart is empty</h1>
+                    <Link to='/shop'><button className="addToCart">SHOP NOW</button></Link>
+                </div>}
+            
 
-                        <div>
-                            <h2>{item.name}</h2><br />
-                            Quantity: <h3>{item.amount}</h3><br />
-                            Total Price: <h3>{item.price * item.amount}</h3>
-                        </div>
-                    </div>
-                );
-            })}
+            <div className="cartInfo">
+                <h1>Cart</h1>
+                <h2>Total: ${total.toFixed(2)}</h2><br />
+                <h2>Items: {totalItems}</h2><br />
+                <button onClick={() => {setCart([])}} className="addToCart">Checkout</button>
+            </div>
         </div>
     );
 }
--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -17,7 +17,7 @@ const useImageURL = () => {
                 return response.json();
             })
             .then((response) => {
-                response = response.slice(0, 9);
+                response = response.slice(0, 19);
                 setImageURL(response);
             })
             .catch((error) => setError(error))
@@ -46,10 +46,19 @@ const Shop = () => {
         setCart(cartCopy);
     }
 
-    let { imageURL, error, loading } = useImageURL();
+    let { imageURL, error, loading } = useImageURL(); 
 
-    if (loading) return <div className="loading"></div>;
-    if (error) return <p>A network error was encountered</p>;
+    if (loading) {
+        return <div className="loading"></div>
+    } else if (error) {
+        return <p>A network error was encountered</p>
+    } else {
+        imageURL = imageURL.filter(function (item) {
+            if (item.category === "men's clothing" || item.category === "women's clothing") {
+                return true;
+            }
+        });
+    }
 
     return (        
         <div className="shop">
